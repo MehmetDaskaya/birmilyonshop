03849d70a6506f109bdb0e4a944654c87e530121
Nerimb <mehmettaskaya15@gmail.com>
2022-12-12 00:29:49 +0300

    Deleted some unused props

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -17,7 +17,7 @@ import Loading from "../../components/Loading/Loading";
 import Error from "../../components/Error";
 import Button from "../../components/Button";
 
-const Detail = ({ route, product, addToCart }) => {
+const Detail = ({ route }) => {
   const piece = useSelector(selectCartPiece);
   const totalSum = useSelector(selectTotalSum);
   const dispatch = useDispatch();
