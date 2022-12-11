2bbaf3cba3d324c4ef9fd6583343810414e74a45
Nerimb <mehmettaskaya15@gmail.com>
2022-12-12 00:34:57 +0300

    Last touches to the task

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -25,15 +25,15 @@ const Cart = () => {
         <Text style={styles.priceBox}>${totalSum} in Total</Text>
       </View>
 
-      <FavoritesCard data={cartSelector} />
+      <ItemCard data={cartSelector} />
     </View>
   );
 };
 
 export default Cart;
 
-const FavoritesCard = ({ data }) => {
-  const renderItem = ({ item }) => <FavoritesCardItem item={item} />;
+const ItemCard = ({ data }) => {
+  const renderItem = ({ item }) => <CardItem item={item} />;
   return (
     <FlatList
       data={data}
@@ -43,7 +43,7 @@ const FavoritesCard = ({ data }) => {
   );
 };
 
-const FavoritesCardItem = ({ item }) => {
+const CardItem = ({ item }) => {
   return (
     <TouchableWithoutFeedback>
       <View style={styles.container}>
@@ -60,58 +60,3 @@ const FavoritesCardItem = ({ item }) => {
     </TouchableWithoutFeedback>
   );
 };
-
-// import { View, Text, FlatList, Image } from "react-native";
-// import React from "react";
-// import styles from "./Cart.style";
-// import {
-//   selectCartPiece,
-//   selectTotalSum,
-//   selectCart,
-// } from "../../../store/slices/cartSlice";
-// import { useSelector } from "react-redux";
-
-// const Cart = () => {
-//   const cartSelector = useSelector(selectCart);
-//   const piece = useSelector(selectCartPiece);
-//   const totalSum = useSelector(selectTotalSum);
-
-//   return (
-//     <View style={styles.container}>
-//       <View>
-//         <Text>{piece} Items in the Cart</Text>
-//       </View>
-
-//       <View>
-//         <Text>${totalSum} in Total</Text>
-//       </View>
-
-//       <CartItemCard data={cartSelector} />
-//     </View>
-//   );
-// };
-
-// export default Cart;
-
-// const CartItemCard = ({ data }) => {
-//   const renderItem = ({ item }) => <CartItem item={item} />;
-//   return (
-//     <FlatList
-//       data={data}
-//       keyExtractor={(item) => item.id}
-//       renderItem={renderItem}
-//     />
-//   );
-// };
-
-// const CartItem = ({ item }) => {
-//   return (
-//     <View style={styles.container}>
-//       <Image style={styles.image} source={{ uri: item.image }} />
-//       <View style={styles.bodyContainer}>
-//         <Text style={styles.title}>{item.title}</Text>
-//         <Text style={styles.price}>${item.price}</Text>
-//       </View>
-//     </View>
-//   );
-// };
